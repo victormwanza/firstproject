aa475a5a7e76e0868ec5bb54234144a9fce31e79
Victor Mwanza <mwanzav1@gmail.com>
2018-09-02 17:34:48 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "firstproject"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,5 @@
+object Main extends App {
+  println(" Hello World ")
+  println(" My First Name is Victor ")
+
+}
